8bbab3dc3d8ef881170aa7d2ebeeb885c4752c74
Adornadowilliam2 <adornadowilliam@gmail.com>
2025-01-20 17:35:12 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+
+import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Login from './pages/Login';
+import Home from './pages/Home';
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Login />
+    },
+    {
+      path: '/home',
+      element: <Home />
+    }
+  ])
+
+
+  return (
+    <RouterProvider router={router} />
+  );
+}
+
+export default App
