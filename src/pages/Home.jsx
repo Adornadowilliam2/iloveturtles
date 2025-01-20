fb20f3723d88a5cdf5631a43941f128b04a3e4a8
Adornadowilliam2 <adornadowilliam@gmail.com>
2025-01-20 19:21:47 +0800

    - add home design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,73 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div>
+      <nav className="p-2 text-white bg-[#007bff] flex items-center justify-between">
+        <div>Turtles</div>
+        <ul className="flex gap-4">
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+      <main className='bg-[url("https://th.bing.com/th/id/OIP.6lpn4Z2LkvzbsdVAlIPbJgHaFj?rs=1&pid=ImgDetMain")] h-[80vh] bg-no-repeat bg-center bg-cover flex justify-center items-center'>
+        <h1 className="bg-[#f0eeeeb7] p-3 lg:text-[3rem] sm:text-[1rem]">
+          Love the Turtles
+        </h1>
+      </main>
+      <section className="flex flex-wrap items-stretch">
+        <div className="w-[200px] block m-auto mt-2 border border-black p-4 hover:bg-[#ccc]">
+          <img
+            src="https://th.bing.com/th/id/OIP.0-Wdcy2haE6rQMSRS5xq9gHaHa?rs=1&pid=ImgDetMain"
+            alt="i like turtle meme image"
+          />
+          <h2 className="uppercase font-bold">i like turtle</h2>
+          <h3 className="text-[14px]">meme video</h3>
+          <h4 className="text-[12px]">
+            click here for video{" "}
+            <a
+              href="https://youtu.be/KWotO76SuXE"
+              className="text-[#007bff] border-none"
+            >
+              https://youtu.be/KWotO76SuXE
+            </a>
+          </h4>
+        </div>
+        <div className="w-[200px] block m-auto mt-2 border border-black p-4 hover:bg-[#ccc]">
+          <img
+            src="https://i1.sndcdn.com/artworks-ibRanO3vLofKkkZ2-dx6oXA-t500x500.jpg"
+            alt="i like turtle meme image"
+          />
+          <h2 className="uppercase font-bold">Squirtle saxophone</h2>
+          <h3 className="text-[14px]">meme video</h3>
+          <h4 className="text-[12px]">
+            click here for video{" "}
+            <a
+              href="https://www.youtube.com/watch?v=D_m-G5vt41s"
+              className="text-[#007bff] border-none"
+            >
+              https://www.youtube.com/watch?v=D_m-G5vt41s
+            </a>
+          </h4>
+        </div>
+        <div className="w-[200px] block m-auto mt-2 border border-black p-4 hover:bg-[#ccc]">
+          <img
+            src="https://th.bing.com/th/id/OIP.Zrw0aKBNu-IdozTfmkA8HQAAAA?w=360&h=360&rs=1&pid=ImgDetMain"
+            alt="i like turtle meme image"
+          />
+          <h2 className="uppercase font-bold">turtle huh</h2>
+          <h3 className="text-[14px]">meme video</h3>
+          <h4 className="text-[12px]">
+            click here for video{" "}
+            <a
+              href="https://www.youtube.com/watch?v=Yx6s8bg3mk4"
+              className="text-[#007bff] border-none"
+            >
+              https://www.youtube.com/watch?v=Yx6s8bg3mk4
+            </a>
+          </h4>
+        </div>
+      </section>
+    </div>
+  );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,45 +1,55 @@
 import React from 'react'
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
+  const navigate = useNavigate();
   return (
     <div className="flex items-center justify-center h-[100vh] font-serif">
-      <div className="border border-black p-4 rounded-lg">
+      <div className="border border-black p-4 rounded-lg flex items-center justify-evenly gap-1 relative">
+        <span
+          className="absolute top-0 right-0 m-1 border border-[#ccc] p-1 rounded-lg hover:cursor-pointer"
+          onClick={() => navigate("/home")}
+        >
+          X
+        </span>
         <img
           src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/38b8a1db-9e65-43fc-b026-1d314208a66b/dfriwo9-cda15585-36ce-4215-9c6b-30266f0be769.png/v1/fill/w_894,h_894,q_70,strp/cute_baby_sea_turtle___under_the_sea_by_beachbumpics_dfriwo9-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTI4MCIsInBhdGgiOiJcL2ZcLzM4YjhhMWRiLTllNjUtNDNmYy1iMDI2LTFkMzE0MjA4YTY2YlwvZGZyaXdvOS1jZGExNTU4NS0zNmNlLTQyMTUtOWM2Yi0zMDI2NmYwYmU3NjkucG5nIiwid2lkdGgiOiI8PTEyODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.8yMBW2OytmgNqA5iq1io1QpiaaThw8YYpUHEg2IMnDA"
           alt="cute turle image"
-          className="w-[100px] block m-auto mb-2 rounded-md"
+          className="w-[200px] xs:hidden md:block"
         />
-        <h1 className="text-center">Register</h1>
-        <div className="mt-2">
-          <label htmlFor="username" className="block">
-            Username:
-          </label>
-          <input
-            type="text"
-            required
-            placeholder="Username"
-            className="border border-black p-2 w-[100%]"
-          />
-        </div>
-        <div className="mt-2">
-          <label htmlFor="username" className="block">
-            Password:
-          </label>
-          <input
-            type="password"
-            required
-            placeholder="Password"
-            className="border border-black p-2 w-[100%]"
-          />
-        </div>
-        <div className="bg-[#007bff] text-white p-2 rounded-lg mt-2 text-center">
-          <button type="submit">Submit</button>
-        </div>
-        <div className="text-[12px] text-center mt-2">
-          <a href="">
-            Already has an account?{" "}
-            <span className="text-[#007bff]">Register</span>
-          </a>
+        <div>
+          <h1 className="text-center">Register</h1>
+          <div className="mt-2">
+            <label htmlFor="username" className="block">
+              Username:
+            </label>
+            <input
+              type="text"
+              required
+              placeholder="Username"
+              className="border border-black p-2 w-[100%]"
+            />
+          </div>
+          <div className="mt-2">
+            <label htmlFor="username" className="block">
+              Password:
+            </label>
+            <input
+              type="password"
+              required
+              placeholder="Password"
+              className="border border-black p-2 w-[100%]"
+            />
+          </div>
+          <div className="bg-[#007bff] text-white p-2 rounded-lg mt-2 text-center">
+            <button type="submit">Submit</button>
+          </div>
+          <div className="text-[12px] text-center mt-2">
+            <a href="">
+              Already has an account?{" "}
+              <span className="text-[#007bff]">Register</span>
+            </a>
+          </div>
         </div>
       </div>
     </div>
